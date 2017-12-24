b06d7dd43cd58cab2e39aa6564dc9077deb1fc88
vpez <vaheh.pezeshkian@gmail.com>
2017-12-25 00:01:58 +0400

    Update customer-queries.sql

--- customer-queries.sql
+++ customer-queries.sql
@@ -24,4 +24,32 @@ SELECT SUM(A.total) AS t, customers.city
 FROM customers NATURAL JOIN
 ( SELECT customerNumber, COUNT(*) AS total
   FROM orders GROUP BY customerNumber) AS A
-GROUP BY city ORDER BY t DESC
+GROUP BY city ORDER BY t DESC;
+
+-- Query 4
+-- Percent of completed orders
+SELECT ROUND(100 * shipped / total, 0) AS precent
+FROM
+  (SELECT COUNT(*) shipped
+   FROM orders
+   WHERE status = 'Shipped') AS S,
+  (SELECT COUNT(*) total
+   FROM orders) AS T;
+   
+-- Query 5
+-- Show list of cities that have more than 10 items shipped.
+SELECT
+  customers.city,
+  SUM(T.c) AS total
+FROM customers
+  NATURAL JOIN
+  (SELECT
+     status,
+     customerNumber,
+     COUNT(*) c
+   FROM orders
+   WHERE status = 'Shipped'
+   GROUP BY customerNumber) AS T
+GROUP BY customers.city
+HAVING total > 10
+ORDER BY total DESC;
